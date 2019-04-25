aff25676ed19682eb711b7f0555be688101c1512
lluong <lamonte.luong@gmail.com>
2019-04-25 19:03:18 -0400

    Adding & deleting to-do items

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,19 +1,27 @@
 // Check off specific items by clicking
-$("li").click(function() {
+$("ul").on("click", "li", function() {
     $(this).toggleClass("completed");
 });
 
 //Toggle text input box
 $(".fa-plus").click(function() {
-    console.log("gottem");
+    $("input[type='text']").fadeToggle();
 });
 
 // Add new todo item to the list
-$("input").keypress(function(e) {
+$("input[type='text']").keypress(function(e) {
     if (e.which == 13) {
         if ($(this).val() != "") {
             $("ul").append("<li> <span><i class=\"fas fa-trash\"></i></span> "+$(this).val()+" </li>")
         }
         $(this).val("");
     }
+});
+
+// Delete an item
+$("ul").on("click", "span", function(e) {
+    $(this).parent().fadeOut(400, function(){
+        $(this).remove();
+    });
+    e.stopPropagation();
 });
